434e776e98bc9299b2c282c3b1848d86cb1b9eec
harshxagarwal <61138680+harshxagarwal@users.noreply.github.com>
2020-04-16 10:03:46 +0530

    Made Changes in 'MainActivity.java'

--- app/src/main/java/com/example/apple/MainActivity.java
+++ app/src/main/java/com/example/apple/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.apple;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.animation.Animation;
+
+import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.myToolbar);
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        viewPager = (ViewPager) findViewById(R.id.myViewPager);
+        setSupportActionBar(toolbar);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+
     }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
+
+    private void setupViewPager(ViewPager viewPager){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new iPhones(),"iPhone");
+        viewPagerAdapter.addFragment(new iPad(),"iPad");
+        viewPagerAdapter.addFragment(new Macintosh(),"Macintosh");
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+
+
+private void TestUpload(){
+        int i=1+1;
+        int ii =2+2;
+        int iii = i+ii;
+}
 }
